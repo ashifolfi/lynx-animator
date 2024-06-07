8bd77026fae9258a347af7fc65069b0e9d017834
K J <anticream39@yahoo.com>
2024-06-07 16:38:18 -0400

    feat: en_US translation file

--- res/lang/LynxAnimator_en_US.ts
+++ res/lang/LynxAnimator_en_US.ts
@@ -1,3 +1,171 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../dialogs/aboutdialog.cpp" line="27"/>
+        <source>Created by %1</source>
+        <translation>Created by %1</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/aboutdialog.cpp" line="36"/>
+        <source>Credits</source>
+        <translation>Credits</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/aboutdialog.cpp" line="48"/>
+        <source>Unable to load license text!</source>
+        <translation>Unable to load license text!</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/aboutdialog.cpp" line="51"/>
+        <source>License</source>
+        <translation>License</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="50"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="65"/>
+        <source>Edit</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="77"/>
+        <source>View</source>
+        <translation>View</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="82"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="95"/>
+        <source>Tools</source>
+        <translation>Tools</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="104"/>
+        <source>About Animator</source>
+        <translation>About Animator</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="109"/>
+        <source>New Project</source>
+        <translation>New Project</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="114"/>
+        <source>Open Project</source>
+        <translation>Open Project</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="119"/>
+        <source>Save Project</source>
+        <translation>Save Project</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="124"/>
+        <source>Save Project As</source>
+        <translation>Save Project As</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="129"/>
+        <source>Close Project</source>
+        <translation>Close Project</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="134"/>
+        <source>Close All</source>
+        <translation>Close All</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="139"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="144"/>
+        <source>Undo</source>
+        <translation>Undo</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="149"/>
+        <source>Redo</source>
+        <translation>Redo</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="154"/>
+        <source>Cut</source>
+        <translation>Cut</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="159"/>
+        <source>Copy</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="164"/>
+        <source>Paste</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="169"/>
+        <source>Delete</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="174"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+</context>
+<context>
+    <name>NewProjectDialog</name>
+    <message>
+        <location filename="../../dialogs/newprojectdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/newprojectdialog.ui" line="48"/>
+        <source>Height:</source>
+        <translation>Height:</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/newprojectdialog.ui" line="55"/>
+        <source>Width:</source>
+        <translation>Width:</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/newprojectdialog.ui" line="86"/>
+        <source>Preset:</source>
+        <translation>Preset:</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectStageView</name>
+    <message>
+        <location filename="../../widgets/projectstageview.cpp" line="18"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/projectstageview.cpp" line="18"/>
+        <source>Unable to initialize OpenGL 3.2 Core context! Please upgrade your computer to preview models.</source>
+        <translation>Unable to initialize OpenGL 3.2 Core context! Please upgrade your computer to preview models.</translation>
+    </message>
+</context>
+</TS>
